2611b792ec27706465b6aedf2ac160e3c6e648cb
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-06 21:43:32 +0900

    react query

---
+++ src/components/ReactQuery.tsx
@@ -0,0 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+type Album = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
+const fetchAlbums = async () => {
+  const result = await axios.get<Album[]>(
+    "https://jsonplaceholder.typicode.com/albums"
+  );
+  return result.data;
+};
+
+export const ReactQuery = () => {
+  const { isLoading, error, data } = useQuery<Album[]>(["albums"], fetchAlbums);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+
+  return (
+    <div>
+      <p>ReactQuery</p>
+      {data?.map((album) => (
+        <div key={album.id}>{album.title}</div>
+      ))}
+    </div>
+  );
+};

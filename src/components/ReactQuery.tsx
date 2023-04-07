ebdaf33604d6f9bfc594f1996543716c4ebb4b8e
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-07 13:17:04 +0900

    suspence & transition

--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -2,21 +2,55 @@ import { Sidebar } from "./Sidebar";
 import { AlbumList } from "./AlbunList";
 import { TodoList } from "./TodoList";
 import { ErrorBoundary } from "react-error-boundary";
-import { Suspense } from "react";
+import { Suspense, useState, useTransition } from "react";
+
+type Tabs = "album" | "todo";
 
 export const ReactQuery = () => {
+  const [selectedTab, setSelectedTab] = useState<Tabs>("todo");
+  const [isPending, startTransition] = useTransition();
+
+  const onClickTabButton = (tab: Tabs) => {
+    startTransition(() => {
+      setSelectedTab(tab);
+    });
+  };
+
+  const buttonStyle = {
+    padding: "12px",
+    fontSize: "16px",
+    border: "none",
+    opacity: isPending ? 0.5 : 1,
+  };
+  const albumButtonStyle = {
+    ...buttonStyle,
+    background: selectedTab === "album" ? "gray" : "white",
+  };
+  const todoButtonStyle = {
+    ...buttonStyle,
+    background: selectedTab === "todo" ? "gray" : "white",
+  };
+
   return (
     <div style={{ display: "flex", padding: "16px" }}>
       <Sidebar />
       <div style={{ flexGrow: 1 }}>
+        <button
+          style={albumButtonStyle}
+          onClick={() => onClickTabButton("album")}
+        >
+          Album
+        </button>
+        <button
+          style={todoButtonStyle}
+          onClick={() => onClickTabButton("todo")}
+        >
+          Todo
+        </button>
+
         <ErrorBoundary fallback={<div>Error!</div>}>
-          <Suspense fallback={<div>Albums Loading...</div>}>
-            <AlbumList />
-          </Suspense>
-        </ErrorBoundary>
-        <ErrorBoundary fallback={<div>Error!</div>}>
-          <Suspense fallback={<div>Todos Loading...</div>}>
-            <TodoList />
+          <Suspense fallback={<div>Loading...</div>}>
+            {selectedTab === "todo" ? <TodoList /> : <AlbumList />}
           </Suspense>
         </ErrorBoundary>
       </div>
